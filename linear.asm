15e3434c6ffa78cd98aa9c2aca9e1e2cbd8fcaf4
Mahadevan Reji <rmahadevan574@gmail.com>
2025-10-27 17:46:14 +0530

    refactor linear search implementation in assembly language for improved input handling and user prompts

--- linear.asm
+++ linear.asm
@@ -1,107 +1,123 @@
-; linear search in assembly language
-
 .model small
 .stack 100h
 
 .data 
-    arr dw 10,20,30,40
-
-    index dw ?
-
-    read_num_msg db "enter number: $"
-    found_msg db " found at: $"
-    not_found_msg db "item not found!$"
+    msg1 db "enter number to be searched: $"
+    arr dw 20 dup(?)
+    msg2 db 13,10,"number found at position: $"
+    msg3 db 13,10,"number not found $"
+    msg4 db "enter number of elements: $"
+    msg5 db "enter element: $"
+    count dw ?
 
 .code 
-
 start:
     mov ax,@data
     mov ds,ax
 
-    call read_number
-    
-    mov bx,0
-    mov cx,4
-    
 
-    lea si,arr ; loads the starting address of arr(array) into si(source index)
+    lea dx,msg4
+    mov ah,09h
+    int 21h
+
+    call read_num
+    mov count,ax
+    mov cx,ax
+    lea di,arr
+
+input_arr_loop:
+    lea dx,msg5
+    mov ah,09h
+    int 21h
+    call read_num
+    mov [di],ax
+    add di,2
+    dec dx
+    loop input_arr_loop
+
+    lea dx,msg1
+    mov ah,09h
+    int 21h
+    call read_num
+
+    lea si,arr
+    mov bx,0
+    mov cx,count
 
 find_loop:
-    cmp [si], ax
+    cmp ax,[si]
     je found
-
-    add si,2 ; add 2 bytes to point to next word i.e 10->20 or 20->30 and so on
-    inc bx ;increment bx by 1 i.e bx=bx+1
-
+    inc bx
+    add si,2
     loop find_loop
-
 not_found:
-    lea dx,not_found_msg
+    lea dx,msg3
     mov ah,09h
     int 21h
     jmp exit
 
 found:
-    mov index,bx
-    call print_num
-    lea dx, found_msg
+    lea dx,msg2
     mov ah,09h
     int 21h
-    mov ax,index
+    mov ax,bx
     call print_num
 
 exit:
     mov ah,4ch
     int 21h
 
-read_number proc
-    lea dx, read_num_msg
-    mov ah,09h
-    int 21h
-
+read_num proc
     mov bx,0
-    mov cx,10
-read_num_loop:
+    mov dx,10
+
+read_loop:
     mov ah,01h
     int 21h
 
     cmp al,13
     je done_input
-
-    sub al,30h
+    sub al,"0"
     mov ah,0
     push ax
+
     mov ax,bx
-    mul cx
+    mul dx
     mov bx,ax
     pop ax
     add bx,ax
-    jmp read_num_loop
+    jmp read_loop
+
 done_input:
     mov ax,bx
     ret
-read_number endp
+read_num endp
 
 print_num proc
-    mov cx,0
     mov bx,10
+    mov cx,0
 
-push_loop:
+convert_loop:
     xor dx,dx
+    cmp ax,0
+    je print_loop
     div bx
-    add dl,30h
+    add dl,"0"
     push dx
     inc cx
-    cmp ax,0 
-    jne push_loop
+    jmp convert_loop
+
 print_loop:
+    cmp cx,0
+    je done_print
     pop dx
     mov ah,02h
     int 21h
     dec cx
-    jnz print_loop
+    jmp print_loop
+
+done_print:
     ret
 print_num endp
-
 
 end start
